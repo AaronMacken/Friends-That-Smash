d0b3e24112fbfa97bacec8e9d2ebb54786ccf796
Aaron Macken <aaron.a.macken@gmail.com>
2019-04-13 17:01:57 -0700

    Cleaned up the CSS file a bit. Adding styling components to the infoDiv

--- FTS.about.js
+++ FTS.about.js
@@ -1,18 +1,34 @@
 var cards = document.querySelectorAll(".card");
 var infoDiv = document.querySelector(".infoDiv");
 
-// Display info Div
+
+// click event listeners for cards
 for (var i = 0; i < cards.length; i++) {
     cards[i].addEventListener("click", function () {
-        infoDiv.style.display = "inline-block";
+        displayInfoDiv();
     });
 }
 
-window.addEventListener("mouseup", function(event){
-    if(event.target != infoDiv){
-        infoDiv.style.display = "none";
+
+
+// && event.target.parentNode != box (to prevent box from being closed when user interacts with it)
+window.addEventListener("mouseup", function (event) {
+    if (event.target != infoDiv) {
+        hideInfoDiv();
     }
 });
 
 
 
+// functions
+function displayInfoDiv() {
+    infoDiv.style.display = "inline-block";
+}
+
+function hideInfoDiv(){
+    // opacity: 0;
+    // animation: opac 0.8s;
+    // infoDiv.style.opacity = "0";
+    // infoDiv.style.animation = "opac 0.8s";
+    infoDiv.style.display = "none";
+}
