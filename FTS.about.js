ca84687b2715c7451702d1c51a336553ce9bd028
Aaron Macken <aaron.a.macken@gmail.com>
2019-04-14 15:43:51 -0700

    Added different pop up windows for each card click. Code needs to be refactored & some UX features are still to be added.

--- FTS.about.js
+++ FTS.about.js
@@ -1,34 +1,39 @@
 var cards = document.querySelectorAll(".card");
-var infoDiv = document.querySelector(".infoDiv");
+var infoDiv1 = document.querySelector("#div1");
+var infoDiv2 = document.querySelector("#div2");
+var infoDiv3 = document.querySelector("#div3");
 
 
-// click event listeners for cards
-for (var i = 0; i < cards.length; i++) {
-    cards[i].addEventListener("click", function () {
-        displayInfoDiv();
-    });
-}
+var c1 = document.querySelector("#c1");
+var c2 = document.querySelector("#c2");
+var c3 = document.querySelector("#c3");
 
 
+c1.addEventListener("click", function(){
+    infoDiv1.style.display = "inline-block";
+});
 
-// && event.target.parentNode != box (to prevent box from being closed when user interacts with it)
-window.addEventListener("mouseup", function (event) {
-    if (event.target != infoDiv) {
-        hideInfoDiv();
-    }
+c2.addEventListener("click", function(){
+    infoDiv2.style.display = "inline-block";
+});
+
+c3.addEventListener("click", function(){
+    infoDiv3.style.display = "inline-block";
 });
 
 
-
-// functions
-function displayInfoDiv() {
-    infoDiv.style.display = "inline-block";
+function hideInfoDiv() {
+    infoDiv1.style.display = "none";
+    infoDiv2.style.display = "none";
+    infoDiv3.style.display = "none";
 }
 
-function hideInfoDiv(){
-    // opacity: 0;
-    // animation: opac 0.8s;
-    // infoDiv.style.opacity = "0";
-    // infoDiv.style.animation = "opac 0.8s";
-    infoDiv.style.display = "none";
-}
+
+
+// Hide info Div if clicked outside of
+// && event.target.parentNode != box (to prevent box from being closed when user interacts with it)
+window.addEventListener("mouseup", function (event) {
+    if (event.target != infoDiv1 && event.target != infoDiv2 && event.target != infoDiv3) {
+        hideInfoDiv();
+    }
+});
